c731368ad0eeb7c5af85ac30c6f0c021e258a27a
Ben Greene <bgree052@gold.ac.uk>
2020-07-16 04:21:21 +0100

    Add Experience/Education forms

---
+++ client/src/components/dashboard/Experience.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+import Moment from "react-moment";
+import { connect } from "react-redux";
+
+const Experience = ({ experience }) => {
+  return <Fragment>
+      <h2 className="my-2">Experience Credentials</h2>
+      <table className="table">
+          
+      </table>
+  </Fragment>;
+};
+
+Experience.propTypes = {};
+
+export default Experience;
